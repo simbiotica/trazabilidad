941683626c2a5330eb2eda05d41eb152811f29ef
Pedro Garcia <pedro@simbiotica.es>
2014-11-16 12:23:37 +0100

    first commit con cositas

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-trazabilidad
+Trazabilidad
 ============
+
+Explore all data in a PA
+
+
+## To develop
+
+* Node 0.10+
+
+Install globally dependencies:
+
+    npm install -g grunt-cli bower
+
+### To install
+
+    npm install
+
+### To run
+
+For development
+    
+    grunt server
+
+For production
+    
+    grunt build
+
+### Deploy to gh-pages
+
+    grunt deploy
---
+++ app/scripts/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+require.config({
+  baseUrl: './scripts',
+
+  paths: {
+    jquery: '../../bower_components/jquery/dist/jquery',
+    underscore: '../../bower_components/underscore/underscore',
+    backbone: '../../bower_components/backbone/backbone',
+    text: '../../bower_components/requirejs-text/text',
+    Class: '../../bower_components/Class.js/Class',
+    underscoreString: '../../bower_components/underscore.string/lib/underscore.string'
+  },
+
+  shim: {
+    jquery: {
+      exports: '$'
+    },
+    underscore: {
+      exports: '_'
+    },
+    backbone: {
+      deps: ['jquery', 'underscore'],
+      exports: 'Backbone'
+    },
+    Class: {
+      exports: 'Class'
+    },
+    underscoreString: {
+      deps: ['underscore'],
+      exports: '_string'
+    }
+  }
+
+});
+
+require([
+], function() {
+  console.log('Vamosss');
+});
