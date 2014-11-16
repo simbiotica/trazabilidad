51496d152ea2bafc885235ccd24b2bfc97ea7fdf
Clara Linos <claralinos@Claras-MacBook-Air-3.local>
2014-11-16 20:13:36 +0100

    bars

--- app/scripts/sidebar-view.js
+++ app/scripts/sidebar-view.js
@@ -14,7 +14,7 @@ define([
     initialize: function() {
       var self = this;
     	this.products = $(atlasService.getProductsList());
-    	this.$selector = this.$el.find('#productsSelector');
+    	this.$selector = $('#productsSelector');
 
       this.products.each(function() {
           self.$selector.append('<option data =' + this.id + '>'+ this.name +'</option>');
@@ -23,10 +23,17 @@ define([
 
       this.$selector.change(_.bind(this.getData, this));
       this.setListeners();
+
+      this.getData(this.$selector);
     },
 
     getData: function(e) {
-      var name = $(e.currentTarget).val();
+      if (e.currentTarget) {
+        var name = $(e.currentTarget).val();
+      } else {
+        var name = $(e).val();
+      };
+      
       var product = _.findWhere(this.products, {name: name});
 
       atlasService.get({
@@ -51,45 +58,41 @@ define([
         $(data).each(function() {
           if (isNaN(this.export_val) || this.export_val === undefined){
             this.export_val = 0;
-
           }
-            var serie = {
-              name: this.dest_id,
-              data: [this.export_val]
-            };
 
-            exportSeries.push(serie);
+          var serie = {
+            name: this.name,
+            data: [this.export_val]
+          };
 
-          console.log(exportSeries);
+          exportSeries.push(serie);
         });
 
-        $(function () {
-          $('#barContainer').highcharts({
-            chart: {
-                type: 'bar'
-            },
-            title: {
-                text: 'Stacked bar chart'
-            },
-            xAxis: {
-                categories: ['Export']
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: ''
-                }
-            },
-            legend: {
-                reversed: true
-            },
-            plotOptions: {
-                series: {
-                    stacking: 'normal'
-                }
-            },
-            series: exportSeries
-          });
+        $('#barContainer').highcharts({
+          chart: {
+              type: 'bar'
+          },
+          title: {
+              text: ''
+          },
+          xAxis: {
+              categories: ['Export']
+          },
+          yAxis: {
+              min: 0,
+              title: {
+                  text: ''
+              }
+          },
+          legend: {
+              reversed: true
+          },
+          plotOptions: {
+              series: {
+                  stacking: 'normal'
+              }
+          },
+          series: exportSeries
         });
 
       });
